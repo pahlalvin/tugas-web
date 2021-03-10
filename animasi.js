55e81b49f6816ec6a394e9117403c6e2e542d96b
alvin pasmah <78840877+pahlalvin@users.noreply.github.com>
2021-03-10 22:51:36 +0700

    Add files via upload

---
+++ animasi.js
@@ -0,0 +1,7 @@
+const menutoogle = document.querySelector('.menu-toogle input');
+const nav = document.querySelector('nav ul');
+
+menutoogle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+
